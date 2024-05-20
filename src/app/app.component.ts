1c08d65dca44a1e7bea4e18db001134f9f173463
Iacob-Deniss-30127 <denissiacob2002@yahoo.com>
2024-05-20 16:41:18 +0300

    frontend -first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'BookReview';
 }
+/*
+const searchAPI = "https://openlibrary.org/search.json";
+
+let loginForm = document.querySelector('.login-form-container');
+const loginButton = document.querySelector<HTMLButtonElement>('#login-button');
+
+if (typeof document !== 'undefined') {
+  const loginButton = document.querySelector<HTMLButtonElement>('#login-button');
+
+  if (loginButton) {
+      loginButton.onclick = () => {
+          // Your onclick handler logic here
+          loginForm?.classList.toggle('active');
+      };
+  } else {
+      console.error("Login button not found");
+  }
+} else {
+  console.warn("Skipping client-side code in SSR context");
+}*/
